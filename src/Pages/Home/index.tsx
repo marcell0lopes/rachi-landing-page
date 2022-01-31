b8ff543f3af0cebafceadf854aeb71e9c0ff3386
Marcello Lopes <marcell0lopes@outlook.com>
2022-01-31 20:03:57 -0300

    add home page

---
+++ src/Pages/Home/index.tsx
@@ -0,0 +1,16 @@
+import { Navbar } from '../../components/Navbar';
+import { Container } from './styles';
+
+export function Home() {
+  return (
+    <Container>
+      <Navbar />
+      {/* <HeroSection/>
+    <ComoFunciona/>
+    <AppStores/>
+    <NossosPlanos/>
+    <Contato/>
+    <Footer/> */}
+    </Container>
+  );
+}
