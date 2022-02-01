7ee4ff63bd77478071db4bae2c20434a60b26fe9
Marcello Lopes <marcell0lopes@outlook.com>
2022-02-01 14:12:14 -0300

    Hero Section cleanup

--- src/components/HeroSection/index.tsx
+++ src/components/HeroSection/index.tsx
@@ -1,19 +1,23 @@
 import { Container, Content } from './styles';
 import heroImg from '../../assets/img-hero.svg';
+import { Button } from '../Button';
 
 export function HeroSection() {
   return (
     <Container>
-      <Content>
-        <div>
-          <h1>
-            Rachi,
-            <br /> é tudo que você precisa em um só lugar.
-          </h1>
-        </div>
-        <button>Cadastrar-se</button>
+      <main>
+        <Content>
+          <div>
+            <h1>
+              Rachi,
+              <br /> é tudo que você <br />
+              precisa em um só lugar.
+            </h1>
+          </div>
+          <Button label="Cadastrar-se" className="cta" />
+        </Content>
         <img src={heroImg} alt="working from anywhere" />
-      </Content>
+      </main>
     </Container>
   );
 }
--- src/components/HeroSection/styles.ts
+++ src/components/HeroSection/styles.ts
@@ -1,19 +1,30 @@
 import styled from 'styled-components';
 
-export const Container = styled.main`
+export const Container = styled.section`
   background-color: var(--white-200);
-  display: flex;
-  align-items: center;
-  justify-content: center;
+  width: 100%;
   max-height: 640px;
   padding: 0 3rem;
+
+  main {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    max-width: 1200px;
+    margin: 0 auto;
+
+    img {
+      margin-left: 4rem;
+    }
+  }
 `;
 
 export const Content = styled.div`
   display: flex;
-
+  flex-direction: column;
   align-items: flex-start;
   justify-content: space-between;
+  margin-left: 0;
 
   h1 {
     font-size: 3rem;
